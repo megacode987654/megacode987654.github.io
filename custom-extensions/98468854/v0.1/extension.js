bfef63e219d9809fb42769b3bc2280dedf30a0dc
Code-Lyoko <mmszek3000@gmail.com>
2016-02-19 10:02:21 -0500

    Create extension.js

---
+++ custom-extensions/98468854/v0.1/extension.js
@@ -0,0 +1,40 @@
+(function(ext) {
+	ext._shutdown = function() {};
+	ext._getStatus = function() {
+		return {status: 2, msg: 'Ready'};
+	};
+	ext.openLink = function(url) {
+        	if (confirm("This project is attempting to send you to the followind website: " + url + "\nContinue?")) {
+var win = window.open(url, '_blank');
+win.focus();
+}};
+ext.openProject = function(id) {
+	window.location.replace("http://scratch.mit.edu/projects/" + id.toString());
+}
+ext.ifThenElse = function(truefalse, a, b) {
+if (truefalse) {
+return a;
+}
+return b;
+}
+ext.power = function(x, y) {
+return Math.pow(x, y);
+}
+ext.destroyScratch = function() {
+	$('body').text('')
+}
+ext.whenThis = function(thingy) {
+return thingy;
+}
+	var descriptor = {
+		blocks: [
+			['', 'open link %s', 'openLink', 'http://example.org/'],
+['f', 'open project id: %n', 'openProject', 98468056],
+['r', 'if %b then %s else %s', 'ifThenElse', null, '', ''],
+['r', '%n ^ %n', 'power', 2, 3],
+['f', 'cause Scrach to self-destruct', 'destroyScratch'],
+['h', 'when %b', 'whenThis', null],,
+        	]
+	};
+	ScratchExtensions.register('Code: More-Blocks', descriptor, ext);
+})({});
